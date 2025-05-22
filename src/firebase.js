cfe341d42447c34e4916d8a8195521f778b04557
Emily Hicks-Rotella <emilyhicks-rotella@macbookpro.mynetworksettings.com>
2025-05-22 09:33:44 -0400

    Fix .env and enable Firebase Auth in local dev

--- src/firebase.js
+++ src/firebase.js
@@ -13,5 +13,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app); // ✅ Add this line
+const storage = getStorage(app); //
 
 export { storage, ref, uploadBytes, getDownloadURL, auth }; // ✅ Export auth
